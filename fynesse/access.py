64a28903076917e0b83af459182423532fdc9a0d
KyraZzz <yz709@cam.ac.uk>
2022-11-25 18:20:33 +0000

    add contents to access and assess

--- fynesse/access.py
+++ fynesse/access.py
@@ -9,11 +9,118 @@ import sqlite"""
 
 import yaml
 from ipywidgets import interact_manual, Text, Password
+import pandas as pd
+import pymysql
+import numpy as np
+from tqdm.notebook import tqdm
 
-@interact_manual(username=Text(description="Username:"), 
-                 password=Password(description="Password:"))
-def write_credentials(username, password):
-    with open("credentials.yaml", "w") as file:
-        credentials_dict = {'username': username, 
-                            'password': password}
-        yaml.dump(credentials_dict, file)
+
+def load_into_database(conn, file_name):
+    """ Load a dataset from the local disk to the MariaDB database
+        via a database connection.
+      :param conn: a database connection
+      :param file_name: the path to a local csv file
+      :return: None
+    """
+    sql = """
+            LOAD DATA LOCAL INFILE %s INTO TABLE `pp_data` 
+            FIELDS TERMINATED BY ',' LINES STARTING BY '' 
+            TERMINATED BY '\n'; 
+         """
+    cur = conn.cursor()
+    try:
+        cur.execute(sql, (file_name, ))
+        conn.commit()
+    except:
+        raise Exception("Unable to load data into database.")
+
+
+def dataset_downloads_uploads(year_lb=1995, year_ub=2022, save_dir="./datasets", verbose=False, conn=None):
+    """ Download the uk price paid dataset (for a period) from gov.uk site 
+        into a local directory, then can optionally upload the files to 
+        a MariaDB database via a database connection.
+      :param year_lb: the time period lower bound (in years, inclusive)
+      :param year_ub: the time period upper bound (in years, inclusive)
+      :param save_dir: a path on the local disk to save the csv files
+      :param verbose: log processing information
+      :param conn: a database connection
+      :return: None
+    """
+    years = np.arange(year_lb, year_ub+1)
+    parts = np.arange(1, 3)
+    general_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
+
+    with tqdm(total=len(years) * len(parts)) as pbar:
+        for year in years:
+            for part in parts:
+                try:
+                    # Try to download the yearly dataset in parts
+                    url = f"{general_url}/pp-{year}-part{part}.csv"
+                    save_path = f"{save_dir}/pp-{year}-part{part}.csv"
+                    # Ignore bad lines and save into a dataframe
+                    df = pd.read_csv(url, on_bad_lines='skip')
+                    # Save the dataframe into a csv on local disk
+                    df.to_csv(save_path, header=False, index=False)
+                    if verbose:
+                        print(
+                            f"save pp-{year}-part{part}.csv into local disk successfully.")
+                    if conn is not None:
+                        load_into_database(conn, save_path)
+                        if verbose:
+                            print(
+                                f"upload pp-{year}-part{part}.csv to table `pp_data` successfully.")
+                    pbar.update(1)
+                except Exception as e:
+                    raise Exception(
+                        f"dataset from year {year} failed to download or upload.")
+
+
+def create_connection(user, password, host, database, port=3306):
+    """ Create a database connection to the MariaDB database
+        specified by the host url and database name.
+    :param user: username
+    :param password: password
+    :param host: host url
+    :param database: database
+    :param port: port number
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = pymysql.connect(user=user,
+                               passwd=password,
+                               host=host,
+                               port=port,
+                               local_infile=1,
+                               db=database
+                               )
+    except:
+        raise Exception(f"Error connecting to the MariaDB Server: {e}")
+    return conn
+
+
+def data():
+    # Database url
+    database_details = {"url": "database-yz709-db.cgrre17yxw11.eu-west-2.rds.amazonaws.com",
+                        "port": 3306}
+
+    @interact_manual(username=Text(description="Username:"),
+                     password=Password(description="Password:"))
+    def write_credentials(username, password):
+        with open("credentials.yaml", "w") as file:
+            credentials_dict = {'username': username,
+                                'password': password}
+            yaml.dump(credentials_dict, file)
+
+    # Get username and password from the yaml file for database access
+    with open("credentials.yaml") as file:
+        credentials = yaml.safe_load(file)
+    username = credentials["username"]
+    password = credentials["password"]
+    url = database_details["url"]
+
+    # Set up a database connection
+    conn = create_connection(user=credentials["username"],
+                             password=credentials["password"],
+                             host=database_details["url"],
+                             database="property_prices")
--- fynesse/assess.py
+++ fynesse/assess.py
@@ -1,16 +1,190 @@
 from .config import *
-
-import .access
-
-"""These are the types of import we might expect in this file
-import pandas
-import bokeh
-import seaborn
+import osmnx as ox
 import matplotlib.pyplot as plt
-import sklearn.decomposition as decomposition
-import sklearn.feature_extraction"""
+import mlai
+import mlai.plot as plot
+import numpy as np
+import pandas as pd
+import access
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
+
+
+def get_bounding_box(latitude, longitude, box_width=0.005, box_height=0.005):
+    """ Construct a bounding box for the point specified in latitude and longitude.
+    :param latitude: the latitude of the point
+    :param longitude: the longitude of the point
+    :param box_width: the width of the bounding box
+    :param box_height: the height of the bounding box
+    :return: the north, south, west and east coordinates for the bounding box
+    """
+    north = latitude + box_height/2
+    south = latitude - box_height/2
+    west = longitude - box_width/2
+    east = longitude + box_width/2
+    return north, south, west, east
+
+
+def get_pois(north, south, west, east):
+    """ Retrieve the point of interests within the bounding box.
+    :param north: the north coordinate of the bounding box
+    :param south: the south coordinate of the bounding box
+    :param west: the west coordinate of the bounding box
+    :param east: the east coordinate of the bounding box
+    :return: POIs in a GeoPandas Dataframe
+    """
+
+    # Define tags from a list of available POIs: https://wiki.openstreetmap.org/wiki/Map_features
+    tags = {"aeroway": True,
+            "amenity": True,
+            "building": True,
+            "highway": True,
+            "landuse": True,
+            "public_transport": True,
+            "historic": True,
+            "leisure": True,
+            "shop": True,
+            "tourism": True,
+            "healthcare": True,
+            "sport": True}
+
+    # Retrieve POIs
+    pois = ox.geometries_from_bbox(north, south, east, west, tags)
+
+    return pois
+
+
+def get_pois_by_key(pois, key):
+    """ Extract relevant POIS with a key.
+    :param pois: POIs in a GeoPandas Dataframe
+    :param key: a column name of the POI
+    :return: POIs in a GeoPandas DataFrame with entries filtered by key
+    """
+    if key not in pois.columns:
+        return None
+
+    pois_by_key = pois[pois[key].notnull()]
+    tags = ["name",
+            "geometry",
+            "addr:city",
+            "addr:postcode"]
+    tags.append(key)
+
+    present_keys = [tag for tag in tags if tag in pois.columns]
+    return pois_by_key[present_keys]
+
+
+def plot_pois(pois, north, south, west, east, place_name="UK", ax=None):
+    """ Plot a graph demonstrating the POIs.
+    :param pois: POIs in a GeoPandas Dataframe
+    :param north: the north coordinate of the bounding box
+    :param south: the south coordinate of the bounding box
+    :param west: the west coordinate of the bounding box
+    :param east: the east coordinate of the bounding box
+    :param place_name: the name of the place
+    :return: a graph demonstrating the POIs
+    """
+    graph = ox.graph_from_bbox(north, south, east, west)
+    # Retrieve nodes and edges
+    nodes, edges = ox.graph_to_gdfs(graph)
+    # Get place boundary related to the place name as a geodataframe
+    area = ox.geocode_to_gdf("UK")
+    if ax is None:
+        fig, ax = plt.subplots(figsize=plot.big_figsize)
+    # Plot the footprint
+    area.plot(ax=ax, facecolor="white")
+    # Plot street edges
+    edges.plot(ax=ax, linewidth=1, edgecolor="dimgray")
+    ax.set_xlim([west, east])
+    ax.set_ylim([south, north])
+    ax.set_xlabel("longitude")
+    ax.set_ylabel("latitude")
+    pic_name = place_name.lower().replace(' ', '-').replace(',', '')
+    ax.set_title(pic_name)
+    ax.ticklabel_format(useOffset=False)
+
+    # Plot all POIs
+    pois.plot(ax=ax, color="blue", alpha=0.7, markersize=10)
+    plt.tight_layout()
+    mlai.write_figure(directory="./maps", filename=f"{pic_name}.svg")
+
+
+def compare_places_pois_by_tag(place_names, locations, box_width, box_height, tags):
+    """ Plot POIs for each tag and each place.
+    :param place_names: a list of strings, each string is a common name of a location
+    :param locations: a list of latitude-longitude pairs, one for each location
+    :param box_width: the width of the bounding box
+    :param box_height: the height of the bounding box
+    :param tags: a list of POI groups
+    :return: one plot per tag per place
+    """
+    fig, ax = plt.subplots(len(tags), len(place_names),
+                           figsize=(len(place_names) * 4, len(tags) * 4))
+    with tqdm(total=len(place_names) * len(tags)) as pbar:
+        for i, p in enumerate(place_names):
+            latitude, longitude = locations[i]
+            north, south, west, east = get_bounding_box(
+                latitude, longitude, box_width, box_height)
+            pois = get_pois(north, south, west, east)
+            for j, t in enumerate(tags):
+                extract_pois = get_pois_by_key(pois, key=t)
+                if extract_pois is not None:
+                    plot_pois(extract_pois, north, south, west,
+                              east, f"{p}-{t}", ax=ax[j][i])
+                pbar.update(1)
+    plt.tight_layout()
+    mlai.write_figure(directory="./maps", filename="compare_places.svg")
+
+
+def count_pois_by_tag(pois):
+    """Count the number of POIs with relevant tags
+    :param pois: POIs in a GeoPandas Dataframe
+    :return: a dictionary (key: tag, value: number of POIs with the relevant tag)
+    """
+    # Define tags from a list of available POIs: https://wiki.openstreetmap.org/wiki/Map_features
+    tags = ["aeroway", "amenity", "building", "highway", "landuse",
+            "public_transport", "historic", "leisure", "shop", "tourism", "healthcare", "sport"]
+    res = {k: 0 for k in tags}
+    for tag in tags:
+        output = get_pois_by_key(pois, tag)
+        res[tag] = len(output) if output is not None else 0
+    return res
+
+
+def get_POIs_for_list(geo_list, box_width=0.005, box_height=0.005):
+    """Iterate through the list of latitude-longitude pairs, count the number
+       of POIs in each tagged group for each location pair.
+    :param geo_list: a list of latitude-longitude pairs
+    :param box_width: the width of the bounding box
+    :param box_height: the height of the bounding box
+    :return: a Dataframe, each row contains the number of POIs in 
+             each tagged group for a location pair
+    """
+    df = pd.DataFrame(columns=['aeroway', 'amenity', 'building', 'highway',
+                               'landuse', 'public_transport', 'historic',
+                               'leisure', 'shop', 'tourism', 'healthcare', 'sport'])
+
+    for idx in tqdm(range(len(geo_list))):
+        latitude, longitude = geo_list[idx]
+        north, south, west, east = get_bounding_box(
+            latitude, longitude, box_width, box_height)
+        pois = get_pois(north, south, west, east)
+        for k, v in count_pois_by_tag(pois).items():
+            df.at[idx, k] = v
+    return df.astype('float64')
+
+
+def get_geo_list(n=10, seed=42):
+    """Get n latitude-longitude pairs.
+    :param n: the number of pairs
+    :param seed: the NumPy random seed, make sure to reproduce the same results
+    :return: a list of n latitude-longitude pairs
+    """
+    np.random.seed(seed)
+    # select latitude range [51, 52] N, longitude range [-0.5, 0.5] E
+    lat_r = np.random.random(n)
+    lon_r = np.random.random(n)
+    return [[51 + i, -0.5 + j] for i, j in zip(lat_r, lon_r)]
 
 
 def data():
@@ -18,13 +192,16 @@ def data():
     df = access.data()
     raise NotImplementedError
 
+
 def query(data):
     """Request user input for some aspect of the data."""
     raise NotImplementedError
 
+
 def view(data):
     """Provide a view of the data that allows the user to verify some aspect of its quality."""
     raise NotImplementedError
+
 
 def labelled(data):
     """Provide a labelled set of data ready for supervised learning."""
