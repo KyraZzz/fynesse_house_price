1d8b41a76b5066a3458a298ce936ebb5a609b683
KyraZzz <yz709@cam.ac.uk>
2022-11-26 00:05:34 +0000

    add three prediction functions into address

--- fynesse/address.py
+++ fynesse/address.py
@@ -232,3 +232,324 @@ def visualise_price_distribution(y, log_y):
     print("Price Skewness: %f" % y.skew())
     print("Log Price Skewness: %f" % log_y.skew())
     plt.show()
+
+
+def train_kfold_eval(X, log_y, n_fold, kfold, model):
+    """ Train and evluate the model with cross validation.
+    :param X: a DataFrame with only independent features
+    :param log_y: log version of the dependent feature (log(prices))
+    :param n_fold: number of folds in cross validation
+    :param kfold: a generator KFold object
+    :param model: the model for curve fitting
+    :return: the average root mean squared error across n folds
+    """
+    cnt = 0
+    rmse_score = 0
+    fig, axes = plt.subplots(1, n_fold, figsize=(n_fold * 4, 4), sharey=True)
+    # Standardise data onto unit scale
+    norm_X = RobustScaler().fit_transform(X)
+    for train_idx, test_idx in kfold.split(X):
+        X_train, X_test = norm_X[train_idx], norm_X[test_idx]
+        y_train, y_test = log_y.iloc[train_idx], log_y.iloc[test_idx]
+        # Model training
+        model_results = model.fit(X_train, y_train)
+        y_pred = model_results.predict(X_test)
+        # Plot results
+        axes[cnt].scatter(np.arange(len(X_test)), np.exp(y_pred), label='pred')
+        axes[cnt].scatter(np.arange(len(X_test)), np.exp(y_test), label='true')
+        axes[cnt].legend()
+        axes[cnt].set_xlabel("Test samples")
+        axes[cnt].set_ylabel("Prices")
+        # Model performance
+        cnt += 1
+        rmse_score += mean_squared_error(np.exp(y_test),
+                                         np.exp(y_pred), squared=False)
+    avg_rmse_score = rmse_score / cnt
+    print(f"Average rmse score with {model}: {avg_rmse_score}")
+    plt.show()
+    return avg_rmse_score
+
+
+def train_predict(X, log_y, X_pred, model):
+    """ Train the model with the whole dataset and predict.
+    :param X: a DataFrame with only independent features
+    :param log_y: log version of the dependent feature (log(prices))
+    :param X_pred: a DataFrame with only independent features for prediction
+    :param model: the model for curve fitting
+    :return: the predicted price for X_pred
+    """
+    # Standardise data onto unit scale
+    norm_X = RobustScaler().fit_transform(X)
+    norm_X_pred = RobustScaler().fit_transform(X_pred)
+    # Model prediction
+    model_results = model.fit(norm_X, log_y)
+    y_pred = model_results.predict(norm_X_pred)
+    return y_pred
+
+
+def train_eval_predict(X, log_y, n_fold, kfold, X_pred, model):
+    """ A pipeline consists of model training, performance evaluation and prediction making.
+    :param X: a DataFrame with only independent features
+    :param log_y: log version of the dependent feature (log(prices))
+    :param n_fold: number of folds in cross validation
+    :param kfold: a generator KFold object
+    :param X_pred: a DataFrame with only independent features for prediction
+    :param model: the model for curve fitting
+    :return: the predicted price for X_pred
+    :return: the average root mean squared error across n folds
+    """
+    avg_rmse_score = train_kfold_eval(X, log_y, n_fold, kfold, model)
+    y_pred = train_predict(X, log_y, X_pred, model)
+    return y_pred, avg_rmse_score
+
+
+def predict_price(latitude, longitude, date, property_type,
+                  train_size_lb=10, train_size_ub=20, train_box_width=0.03,
+                  train_box_height=0.03, diff_lb=20, diff_ub=20, verbose=True):
+    """ Predict the house price for a property at location with 
+       a given latitude-longitude pair at a given date and with a given property type.
+    :param latitude: the latitude of the point
+    :param longitude: the longitude of the point
+    :param date: date in interest
+    :param property_type: a single character describing the property type of the house at the given location,
+                            (e.g., a single character in ['F', 'S', 'D', 'T'])
+    :param train_size_lb: the lower bound of the train dataset size
+    :param train_size_ub: the upper bound of the train dataset size
+    :param train_box_width: the initial bounding box width of the train dataset
+    :param train_box_height: the initial bounding box height of the train dataset
+    :param diff_lb: the initial lower bound of the date range in difference
+    :param diff_ub: the initial upper bound of the date range in difference
+    :param verbose: whether to log processing information
+    :return: a DataFrame with each entry: ['linear_model_name', 'predict_price', 'average_RMSE']
+    """
+    colorList = {'red': '\033[91m', 'green': '\033[92m', 'yellow': '\033[93m'}
+    score_df = pd.DataFrame(
+        columns=['linear_model_name', 'predict_price', 'average_RMSE'])
+
+    try:
+        print(f"{colorList['yellow']}" + "Get database connection...")
+        conn = create_connection(user=credentials["username"],
+                                 password=credentials["password"],
+                                 host=database_details["url"],
+                                 database="property_prices")
+        print(f"{colorList['green']}" +
+              "Database connection set up successfully.")
+    except:
+        raise Exception(f"{colorList['red']}" +
+                        "Unable to connect to the database.")
+
+    try:
+        print(f"{colorList['yellow']}" +
+              f"Get optimal train dataset with size within range: [{train_size_lb}, {train_size_ub}]...")
+        df = get_optimal_train_dataset(conn, latitude, longitude, date,
+                                       property_type, train_size_lb, train_size_ub,
+                                       train_box_width, train_box_height,
+                                       diff_lb, diff_ub, verbose)
+        unique_property = df['property_type'].unique()
+        print(f"{colorList['green']}" + "Optimal train dataset fetched.")
+    except:
+        raise Exception(f"{colorList['red']}" +
+                        "Unable to featch optimal train dataset.")
+
+    try:
+        print(f"{colorList['yellow']}" +
+              f"Preprocess dataframe (merge with OSM features)...")
+        df = preprocess_df(
+            df, columns=['property_type'], unique_vals_columns=[unique_property])
+        print(f"{colorList['green']}" + f"Dataset ready for model training.")
+    except:
+        raise Exception(f"{colorList['red']}" +
+                        "Unable to preprocess the dataframe.")
+
+    try:
+        X, y, n_fold, kfold = kfold_train_test_split(df)
+        log_y = np.log(y)
+        print(f"{colorList['green']}" + f"Visualise price distribution.")
+        visualise_price_distribution(y, log_y)
+        X_pred = get_POIs_for_list([(latitude, longitude)])
+        X_pred['property_type'] = property_type
+        X_pred = preprocess_df(
+            X_pred, columns=['property_type'], unique_vals_columns=[unique_property])
+        print(f"{colorList['green']}" + f"Evalutation dataset constructed.")
+        model_list = [RidgeCV(), BayesianRidge(), TweedieRegressor(power=0)]
+        print(f"{colorList['yellow']}" +
+              "Model training, testing and evaluating ...")
+        for model in model_list:
+            y_pred, avg_rmse_score = train_eval_predict(
+                X, log_y, n_fold, kfold, X_pred, model)
+            score_df = score_df.append({"linear_model_name": str(model), "predict_price": round(
+                np.exp(y_pred[0]), 3), "average_RMSE": avg_rmse_score}, ignore_index=True)
+        print(f"{colorList['green']}" + "Results:")
+        print(score_df)
+    except:
+        raise Exception(
+            f"{colorList['red']}" + "Unable to train, evaluate and test the models.")
+
+    return score_df
+
+
+def predict_price_fix(latitude, longitude, date, property_type, train_box_width=0.03,
+                      train_box_height=0.03, diff_lb=20, diff_ub=20, verbose=True):
+    """ Predict the house price for a property at location with 
+       a given latitude-longitude pair at a given date and with a given property type,
+       fix the bounding box and date range.
+    :param latitude: the latitude of the point
+    :param longitude: the longitude of the point
+    :param date: date in interest
+    :param property_type: a single character describing the property type of the house at the given location,
+                            (e.g., a single character in ['F', 'S', 'D', 'T'])
+    :param train_box_width: the initial bounding box width of the train dataset
+    :param train_box_height: the initial bounding box height of the train dataset
+    :param diff_lb: the initial lower bound of the date range in difference
+    :param diff_ub: the initial upper bound of the date range in difference
+    :param verbose: whether to log processing information
+    :return: a DataFrame with each entry: ['linear_model_name', 'predict_price', 'average_RMSE']
+    """
+    colorList = {'red': '\033[91m', 'green': '\033[92m', 'yellow': '\033[93m'}
+    score_df = pd.DataFrame(
+        columns=['linear_model_name', 'predict_price', 'average_RMSE'])
+
+    try:
+        print(f"{colorList['yellow']}" + "Get database connection...")
+        conn = create_connection(user=credentials["username"],
+                                 password=credentials["password"],
+                                 host=database_details["url"],
+                                 database="property_prices")
+        print(f"{colorList['green']}" +
+              "Database connection set up successfully.")
+    except:
+        raise Exception(f"{colorList['red']}" +
+                        "Unable to connect to the database.")
+
+    try:
+        print(f"{colorList['yellow']}" + f"Get train dataset ...")
+        df = get_train_dataset(conn, latitude, longitude, date, train_box_width,
+                               train_box_height, diff_lb, diff_ub, verbose)
+        unique_property = df['property_type'].unique()
+        print(f"{colorList['green']}" + "Optimal train dataset fetched.")
+    except:
+        raise Exception(f"{colorList['red']}" +
+                        "Unable to featch optimal train dataset.")
+
+    try:
+        print(f"{colorList['yellow']}" +
+              f"Preprocess dataframe (merge with OSM features)...")
+        df = preprocess_df(
+            df, columns=['property_type'], unique_vals_columns=[unique_property])
+        print(f"{colorList['green']}" + f"Dataset ready for model training.")
+    except:
+        raise Exception(f"{colorList['red']}" +
+                        "Unable to preprocess the dataframe.")
+
+    try:
+        X, y, n_fold, kfold = kfold_train_test_split(df)
+        log_y = np.log(y)
+        print(f"{colorList['green']}" + f"Visualise price distribution.")
+        visualise_price_distribution(y, log_y)
+        X_pred = get_POIs_for_list([(latitude, longitude)])
+        X_pred['property_type'] = property_type
+        X_pred = preprocess_df(
+            X_pred, columns=['property_type'], unique_vals_columns=[unique_property])
+        print(f"{colorList['green']}" + f"Evalutation dataset constructed.")
+        model_list = [RidgeCV(), BayesianRidge(), TweedieRegressor(power=0)]
+        print(f"{colorList['yellow']}" +
+              "Model training, testing and evaluating ...")
+        for model in model_list:
+            y_pred, avg_rmse_score = train_eval_predict(
+                X, log_y, n_fold, kfold, X_pred, model)
+            score_df = score_df.append({"linear_model_name": str(model), "predict_price": round(
+                np.exp(y_pred[0]), 3), "average_RMSE": avg_rmse_score}, ignore_index=True)
+        print(f"{colorList['green']}" + "Results:")
+        print(score_df)
+    except:
+        raise Exception(
+            f"{colorList['red']}" + "Unable to train, evaluate and test the models.")
+
+    return score_df
+
+
+def predict_price_relaxed_property(latitude, longitude, date, property_type,
+                                   train_size_lb=10, train_size_ub=20, train_box_width=0.03,
+                                   train_box_height=0.03, diff_lb=20, diff_ub=20, verbose=True):
+    """ Predict the house price for a property at location with 
+       a given latitude-longitude pair at a given date and with a given property type,
+       relax the property type filter of the training dataset.
+    :param latitude: the latitude of the point
+    :param longitude: the longitude of the point
+    :param date: date in interest
+    :param property_type: a single character describing the property type of the house at the given location,
+                            (e.g., a single character in ['F', 'S', 'D', 'T'])
+    :param train_size_lb: the lower bound of the train dataset size
+    :param train_size_ub: the upper bound of the train dataset size
+    :param train_box_width: the initial bounding box width of the train dataset
+    :param train_box_height: the initial bounding box height of the train dataset
+    :param diff_lb: the initial lower bound of the date range in difference
+    :param diff_ub: the initial upper bound of the date range in difference
+    :param verbose: whether to log processing information
+    :return: a DataFrame with each entry: ['linear_model_name', 'predict_price', 'average_RMSE']
+    """
+    colorList = {'red': '\033[91m', 'green': '\033[92m', 'yellow': '\033[93m'}
+    score_df = pd.DataFrame(
+        columns=['linear_model_name', 'predict_price', 'average_RMSE'])
+
+    try:
+        print(f"{colorList['yellow']}" + "Get database connection...")
+        conn = create_connection(user=credentials["username"],
+                                 password=credentials["password"],
+                                 host=database_details["url"],
+                                 database="property_prices")
+        print(f"{colorList['green']}" +
+              "Database connection set up successfully.")
+    except:
+        raise Exception(f"{colorList['red']}" +
+                        "Unable to connect to the database.")
+
+    try:
+        print(f"{colorList['yellow']}" +
+              f"Get optimal train dataset with size within range: [{train_size_lb}, {train_size_ub}]...")
+        property_type_all = ['F', 'S', 'D', 'T']
+        df = get_optimal_train_dataset(conn, latitude, longitude, date,
+                                       property_type_all, train_size_lb, train_size_ub,
+                                       train_box_width, train_box_height,
+                                       diff_lb, diff_ub, verbose)
+        unique_property = df['property_type'].unique()
+        print(f"{colorList['green']}" + "Optimal train dataset fetched.")
+    except:
+        raise Exception(f"{colorList['red']}" +
+                        "Unable to featch optimal train dataset.")
+
+    try:
+        print(f"{colorList['yellow']}" +
+              f"Preprocess dataframe (merge with OSM features)...")
+        df = preprocess_df(
+            df, columns=['property_type'], unique_vals_columns=[unique_property])
+        print(f"{colorList['green']}" + f"Dataset ready for model training.")
+    except:
+        raise Exception(f"{colorList['red']}" +
+                        "Unable to preprocess the dataframe.")
+
+    try:
+        X, y, n_fold, kfold = kfold_train_test_split(df)
+        log_y = np.log(y)
+        print(f"{colorList['green']}" + f"Visualise price distribution.")
+        visualise_price_distribution(y, log_y)
+        X_pred = get_POIs_for_list([(latitude, longitude)])
+        X_pred['property_type'] = property_type
+        X_pred = preprocess_df(
+            X_pred, columns=['property_type'], unique_vals_columns=[unique_property])
+        print(f"{colorList['green']}" + f"Evalutation dataset constructed.")
+        model_list = [RidgeCV(), BayesianRidge(), TweedieRegressor(power=0)]
+        print(f"{colorList['yellow']}" +
+              "Model training, testing and evaluating ...")
+        for model in model_list:
+            y_pred, avg_rmse_score = train_eval_predict(
+                X, log_y, n_fold, kfold, X_pred, model)
+            score_df = score_df.append({"linear_model_name": str(model), "predict_price": round(
+                np.exp(y_pred[0]), 3), "average_RMSE": avg_rmse_score}, ignore_index=True)
+        print(f"{colorList['green']}" + "Results:")
+        print(score_df)
+    except:
+        raise Exception(
+            f"{colorList['red']}" + "Unable to train, evaluate and test the models.")
+
+    return score_df
